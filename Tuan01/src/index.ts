6e952c75571e20eac57c3acd110d565f62a823b1
H82M19\Student <Student>
2025-08-20 07:55:52 +0700

    feat: Week 01, Ex3: Create class Car

--- Tuan01/src/index.ts
+++ Tuan01/src/index.ts
@@ -35,3 +35,17 @@ class Student extends Person {
 const student = new Student("Tran Ngoc Huyen", 20, "DHKTPM18A");
 console.log("==================Cau 2==================")
 student.displayAllInfo();
+
+// 3. Create a class Car with properties brand, model, year. 
+// Write a method to show car info.
+class Car {
+  constructor(public brand: string, public model: string, public year: number) {}
+
+  showCarInfo(): void {
+    console.log(`Brand: ${this.brand}, Model: ${this.model}, Year: ${this.year}`);
+  }
+}
+
+const car = new Car("Vinfast", "VF3", 2025)
+console.log("==================Cau 3==================")
+car.showCarInfo();
