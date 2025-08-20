eedf7b446f7f9b055b9093343c9a0998bb1d8f16
H82M19\Student <Student>
2025-08-20 07:51:38 +0700

    feat: Week 01, Ex1: Create class Person

--- Tuan01/src/index.ts
+++ Tuan01/src/index.ts
@@ -3,3 +3,17 @@ function hello(name: string): string {
 }
 
 console.log(hello("200Lab"));
+
+//1. Create a class Person with attributes name and age. 
+// Write a method to display this information.
+class Person {
+  constructor(public name: string, public age: number) {}
+
+  displayInfo(): void {
+    console.log(`Name: ${this.name}, Age: ${this.age}`);
+  }
+}
+
+const person = new Person("Tran Ngoc Huyen", 20);
+console.log("==================Cau 1==================")
+person.displayInfo();
