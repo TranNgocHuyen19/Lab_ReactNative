ca411b747189082f064212fa260cb121cc98210a
H82M19\Student <Student>
2025-08-20 07:53:53 +0700

    feat: Week 01, Ex2: Create class Student extending Person

--- Tuan01/src/index.ts
+++ Tuan01/src/index.ts
@@ -17,3 +17,21 @@ class Person {
 const person = new Person("Tran Ngoc Huyen", 20);
 console.log("==================Cau 1==================")
 person.displayInfo();
+
+// 2. Write a class Student extending Person with an 
+// additional attribute grade. Add a method to
+// display all info.
+class Student extends Person {
+  constructor(name: string, age: number, public grade: string) {
+    super(name, age);
+  }
+
+  displayAllInfo(): void {
+    this.displayInfo();
+    console.log(`Grade: ${this.grade}`);
+  }
+}
+
+const student = new Student("Tran Ngoc Huyen", 20, "DHKTPM18A");
+console.log("==================Cau 2==================")
+student.displayAllInfo();
